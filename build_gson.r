7512cf21b3360de9fac3c2dc97f4525f62d94bd4
swcyo <swcyo@126.com>
2023-12-24 00:54:01 +0800

    newhtml

--- build_gson.r
+++ build_gson.r
@@ -1,8 +1,7 @@
-
-# GO
 library(clusterProfiler)
 library(org.Hs.eg.db)
 library(gson)
+# GO
 gson_BP_human <- gson_GO(OrgDb = org.Hs.eg.db, keytype = 'ENTREZID', ont = "BP")
 gson_MF_human <- gson_GO(OrgDb = org.Hs.eg.db, keytype = 'ENTREZID', ont = "MF")
 gson_CC_human <- gson_GO(OrgDb = org.Hs.eg.db, keytype = 'ENTREZID', ont = "CC")
@@ -13,13 +12,13 @@ write.gson(gson_CC_human, file = "GO_CC_human.gson")
 write.gson(gson_ALL_human, file = "GO_ALL_human.gson")
 
 # KEGG
-KEGG_human <- gson_KEGG(species = "hsa", KEGG_Type="KEGG", keyType="kegg") 
-MKEGG_human <- gson_KEGG(species = "hsa", KEGG_Type="MKEGG", keyType="kegg") 
+KEGG_human <- gson_KEGG(species = "hsa", KEGG_Type="KEGG", keyType="kegg")
+MKEGG_human <- gson_KEGG(species = "hsa", KEGG_Type="MKEGG", keyType="kegg")
 write.gson(KEGG_human, file = "KEGG_human.gson")
 write.gson(MKEGG_human, file = "MKEGG_human.gson")
 
 # WikiPathways
-WikiPathways_human <- gson_WP("Homo sapiens") 
+WikiPathways_human <- gson_WP("Homo sapiens")
 write.gson(WikiPathways_human, file = "WikiPathways_human.gson")
 
 # Reactome
